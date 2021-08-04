43bd7bf4d27cce061f307f11a660c34b7736996e
wangheyao <1874373481@qq.com>
2021-08-04 14:01:30 +0800

    create

---
+++ Promise.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: WangHeYao
+ * @Date: 2021-08-04 13:58:06
+ * @LastEditTime: 2021-08-04 13:58:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Promise\Promise.js
+ */
