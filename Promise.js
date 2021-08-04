5801669adc01e6e7fac210cd10bdc4a1222228f6
qing-m <1874373481@qq.com>
2021-08-04 14:50:42 +0800

    分析Promise的组成，建造Promise类

--- Promise.js
+++ Promise.js
@@ -1,8 +1,15 @@
 /*
  * @Author: WangHeYao
  * @Date: 2021-08-04 13:58:06
- * @LastEditTime: 2021-08-04 13:58:10
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \Promise\Promise.js
  */
+
+// 首先Promise是这样使用的
+// new Promise((resolve,reject)=>{......})
+
+// 从使用方法来看 Promise 肯定是一个类，我们就用class来声明。
+class Promise {
+  // new Promise 时传入了一个类型为函数的参数 这里就设置为 fn 并且传入就执行
+  constructor(fn) {
+    fn()
+  }
+}
