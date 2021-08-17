38c5ebc75d2372834b42dc46423b4225f11d7265
qing-m <1874373481@qq.com>
2021-08-17 14:18:11 +0800

    完成

--- Promise.js
+++ Promise.js
@@ -13,6 +13,50 @@ const PENDING = 'pending';
 const FULFILLED = 'fulfilled';
 const REJECTED = 'rejected';
 
+function resolvePromise(promise2, x, resolve, reject){
+  // 循环引用报错
+  if(x === promise2){
+    // reject报错
+    return reject(new TypeError('Chaining cycle detected for promise'));
+  }
+  // 防止多次调用
+  let called;
+  // x不是null 且x是对象或者函数
+  if (x != null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      // A+规定，声明then = x的then方法
+      let then = x.then;
+      // 如果then是函数，就默认是promise了
+      if (typeof then === 'function') { 
+        // 就让then执行 第一个参数是this   后面是成功的回调 和 失败的回调
+        then.call(x, y => {
+          // 成功和失败只能调用一个
+          if (called) return;
+          called = true;
+          // resolve的结果依旧是promise 那就继续解析
+          resolvePromise(promise2, y, resolve, reject);
+        }, err => {
+          // 成功和失败只能调用一个
+          if (called) return;
+          called = true;
+          reject(err);// 失败了就失败了
+        })
+      } else {
+        resolve(x); // 直接成功即可
+      }
+    } catch (e) {
+      // 也属于失败
+      if (called) return;
+      called = true;
+      // 取then出错了那就不要在继续执行了
+      reject(e); 
+    }
+  } else {
+    resolve(x);
+  }
+}
+
+
 // 从使用方法来看 Promise 肯定是一个类，我们就用class来声明。
 class Promise {
   // new Promise 时传入了一个类型为函数的参数 这里就设置为 executor 并且传入就执行
@@ -65,32 +109,61 @@ class Promise {
 
   // .then
   then(onFulfilled, onRejected) {
+    // onFulfilled如果不是函数，就忽略onFulfilled，直接返回value
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    // onRejected如果不是函数，就忽略onRejected，直接扔出错误
+    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
     // 根据PromiseA+规范 生命返回的promise2 链式调用
     let promise2 = new Promise((resolve,reject) => {
       // 状态为fulfilled，执行onFulfilled，传入成功的值
       if (this.state === FULFILLED) {
-        let x = onFulfilled(this.value)
-        // resolvePromise函数，处理自己return的promise和默认的promise2的关系
-        resolvePromise(promise2, x, resolve, reject);
+        setTimeout(() => {
+          try {
+            let x = onFulfilled(this.value)
+            // resolvePromise函数，处理自己return的promise和默认的promise2的关系
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error)
+          }
+        },0)
       };
       // 状态为rejected，执行onRejected，传入失败的原因
       if (this.state === REJECTED) {
-        let x = onRejected(this.reason);
-        resolvePromise(promise2, x, resolve, reject);
+        setTimeout(()=>{
+          try {
+            let x = onRejected(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error)
+          }
+        },0)
       };
       if(this.state === PENDING) {
         // onFulfilled传入到成功数组
         this.onResolvedCallbacks.push(()=>{
-          let x = onFulfilled(this.value);
-          resolvePromise(promise2, x, resolve, reject);
+          setTimeout(()=>{
+            try {
+              let x = onFulfilled(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (error) {
+              reject(error)
+            }
+          },0)
         })
         // onRejected传入到失败数组
         this.onRejectedCallbacks.push(()=>{
-          let x = onRejected(this.reason);
-          resolvePromise(promise2, x, resolve, reject);
+          setTimeout(()=>{
+            try {
+              let x = onRejected(this.reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (error) {
+              reject(error)
+            }
+          },0)
         })
       }
     })
+    return promise2;
   }
 }
 
